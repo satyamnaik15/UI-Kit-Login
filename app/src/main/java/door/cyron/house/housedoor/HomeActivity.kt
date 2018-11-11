ab57f82fce3cf2a0690342c0a68ce19ac582736a
subhamnaik15 <subhamnaik15@gmail.com>
2018-11-12 02:23:17 +0530

    Few changes

--- app/src/main/java/door/cyron/house/housedoor/HomeActivity.kt
+++ app/src/main/java/door/cyron/house/housedoor/HomeActivity.kt
@@ -1,5 +1,6 @@
 package door.cyron.house.housedoor
 
+import android.animation.Animator
 import android.animation.ValueAnimator
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
@@ -7,12 +8,17 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.view.ViewTreeObserver.OnGlobalLayoutListener
 import android.view.animation.DecelerateInterpolator
+import android.widget.EditText
+import android.animation.ObjectAnimator
+
+
 
 
 class HomeActivity : AppCompatActivity() {
 
     lateinit var floatingActionButton: FloatingActionButton
     lateinit var view: View
+    lateinit var editText: EditText
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +26,7 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
         floatingActionButton = findViewById(R.id.floatingActionButton2)
         view = findViewById(R.id.view)
+        editText = findViewById(R.id.editText)
 
         val vto = view.viewTreeObserver
         vto.addOnGlobalLayoutListener(object : OnGlobalLayoutListener {
@@ -44,7 +51,25 @@ class HomeActivity : AppCompatActivity() {
             view.requestLayout()
         }
         widthAnimator.start()
+        widthAnimator.addListener(object : Animator.AnimatorListener {
+
+            override fun onAnimationStart(animation: Animator) {
+                // ...
+            }
+
+            override fun onAnimationRepeat(animation: Animator) {
+                // ...
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+            }
+
+            override fun onAnimationCancel(animation: Animator) {
+                // ...
+            }
+        })
     }
+
 
     override fun onBackPressed() {
         super.onBackPressed()
